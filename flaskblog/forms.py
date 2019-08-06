ce21340cf8e51726b02cc50b74988805ef58440d
peter <petermwovi73@gmail.com>
2019-08-06 11:30:08 +0300

    flask login project

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -5,34 +5,29 @@ from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username= StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email=StringField('Email', validators=[DataRequired(), Email()])
-    password=PasswordField('password', validators=[DataRequired()])
-    confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+        username= StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+        email=StringField('Email', validators=[DataRequired(), Email()])
+        password=PasswordField('password', validators=[DataRequired()])
+        confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-    submit=SubmitField('Sign Up')
-
-
-
-def validate_username(self, username):
-
-    user = User.query.filter_by(username=username.data).first()
-
-    if user:
-        raise ValidationError('That username is taken. Please choose  another')
+        submit=SubmitField('Sign Up')
 
 
-def validate_email(self, email):
 
-    user = User.query.filter_by(email=email.data).first()
+        def validate_username(self, username):
+                user = User.query.filter_by(username=username.data).first()
+                if user:
+                        raise ValidationError('Please use a different username.')
 
-    if user:
-        raise ValidationError('That email is taken. Please choose  another')
+        def validate_email(self, email):
+                user = User.query.filter_by(email=email.data).first()
+                if user:
+                        raise ValidationError('Please use a different email address.')
 
 
 class LoginForm(FlaskForm):
-    email= StringField('Email', validators=[DataRequired(), Email()])
-    password= PasswordField('password', validators=[DataRequired()])
-    remember= BooleanField('Remember Me')
+        email= StringField('Email', validators=[DataRequired(), Email()])
+        password= PasswordField('password', validators=[DataRequired()])
+        remember= BooleanField('Remember Me')
 
-    submit= SubmitField('Login')
+        submit= SubmitField('Login')
