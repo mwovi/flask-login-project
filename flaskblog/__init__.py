d585687b7ced22d90a57be1bc1b5f80680c4bad2
peter <petermwovi73@gmail.com>
2019-08-06 15:25:19 +0300

    upating of the database through registration and login implemented

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='21192fcf622691fdcc2a64868add4b3f'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///razo.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 from flaskblog import routes
